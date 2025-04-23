abd7aa76374a15ed47fdc515557824de491791c5
micah-earl <ear24005@byui.edu>
2025-04-23 10:22:25 -0600

    area of a circle

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // this is a program to compute the area of a circle. 
+        // get the radius from the user
+        Console.Write("please enter the radius:");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        //compute time 
+
+        double area = Math.PI * radius * radius;
+      
+        
+
+       Console.WriteLine($"Area of the circle: {Math.Round(area, 2)}");
     }
 }
