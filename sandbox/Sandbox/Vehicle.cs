05037cb8e412ac8d71e613aff4cc5f2dd72b55ac
micah-earl <ear24005@byui.edu>
2025-06-04 11:07:26 -0600

    prep 08

---
+++ sandbox/Sandbox/Vehicle.cs
@@ -0,0 +1,42 @@
+public class Vehicle
+{
+    private int _yearManufactured;
+    private string _manufacture;
+    private string _modalname;
+
+    public Vehicle(int yearManufactured, string manufacture, string modalname)
+    {
+        _yearManufactured = yearManufactured;
+        _manufacture = manufacture;
+        _modalname = modalname;
+
+    }
+
+    public int GetYearManufactured()
+    {
+        return _yearManufactured;
+    }
+}
+
+public Car : Vehicle
+{
+ private int _numberOfDoors;
+
+ public Car(int yearManufactured, string manufacture, string modalname, int numberOfDoors)
+ : base()
+}
+(int yearManufactured, )
+
+public class ford : Car
+{
+    public ford(int yearManufactured, string modalname, int numberOfDoors)
+
+}
+
+public class program2
+{
+    public static void Main(string[] args)
+    {
+        Car car1 = new Car(2006, Hondai)
+    }
+}
